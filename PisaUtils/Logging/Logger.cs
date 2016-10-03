6331fdf3e10ae55e972da04b46cf7a40143bedfa
psaenz <psaenz@gmail.com>
2016-10-02 18:20:21 -0600

    Adding a utility to work with types

--- PisaUtils/Logging/Logger.cs
+++ PisaUtils/Logging/Logger.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Pisa.Utils.Logging
 {
